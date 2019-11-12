123fe5b2f71c7263c9992ccad22e2d19ec7c330a
Nick Garner <nrgarner@ncsu.edu>
2019-11-12 12:16:03 -0500

    Added repo to Eclipse, created main Kruskal.java

---
+++ src/proj3/Kruskal.java
@@ -0,0 +1,26 @@
+package proj3;
+
+/**
+ * Main class
+ * @author Nick Garner, nrgarner
+ *
+ */
+public class Kruskal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private class Vertex {
+		
+	}
+	
+	private class Edge {
+		
+	}
+	
+	private class Heap <E> {
+		
+	}
+}
